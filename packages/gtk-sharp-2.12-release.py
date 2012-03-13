692d6b0570bbf17f4cf6aafbfadbeec9b8028f9b
Alex Corrado <alexc@xamarin.com>
2012-03-13 14:25:35 -0400

    Revert "Make gtk-sharp-2.12-release package make install verbose to try to defeat wrench 15 min no output timeout"
    This reverts commit 79f52096f1de60562623d7050d0e8355132c8eef.
    
    Didn't help.

--- packages/gtk-sharp-2.12-release.py
+++ packages/gtk-sharp-2.12-release.py
@@ -4,6 +4,5 @@ class GtkSharp212ReleasePackage (Package):
 		# self.configure = './bootstrap-2.12 --prefix="%{prefix}"'
 		self.sources = ['http://download.mono-project.com/sources/gtk-sharp212/gtk-sharp-2.12.11.tar.bz2']
 		self.make = 'make CSC=gmcs'
-		self.makeinstall = 'make install VERBOSE=1'
 
 GtkSharp212ReleasePackage ()
